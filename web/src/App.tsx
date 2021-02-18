cf323b77525752b4bcf25fa3e7ab60c374e554e9
saiumesh535 <d.sai535@gmail.com>
2021-02-18 20:37:51 +0530

    build fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -4,12 +4,12 @@ import { Statuses, getStatusesAPI } from './api/api';
 
 function App() {
 
-  const [statuses, setStatuses] = useState<Statuses[]>([]);
+  const [statuses, setStatuses] = useState<Statuses>({});
 
   async function getStatuses() {
     try {
-      const statuses = await getStatusesAPI();
-      setStatuses(statuses);
+      const statusesResp = await getStatusesAPI();
+      setStatuses(statusesResp);
     } catch (error) {
       console.error(error);
     }
@@ -21,6 +21,12 @@ function App() {
 
   return (
     <div className="App">
+      {Object.keys((statuses)).map((key) => (
+        <div className="status">
+          <p>Name: {key}</p>
+          <p>Status: {statuses[key]}</p>
+        </div>
+      ))}
     </div>
   );
 }
--- web/src/api/api.ts
+++ web/src/api/api.ts
@@ -4,7 +4,7 @@ export interface Statuses {
     [key: string]: string;
 }
 
-export async function getStatusesAPI(): Promise<Statuses[]> {
+export async function getStatusesAPI(): Promise<Statuses> {
     const resp = await axios.get('http://awsstatus-env.eba-kfp7zb2p.us-east-1.elasticbeanstalk.com/');
     return resp.data;
 }
